8ff245a18b0175a6d1c6b377825767b33dffa753
ardenzhao <iguoxing@163.com>
2022-02-23 17:15:11 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: ArdenZhao
+ * @Date: 2022-02-23 17:08:27
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2022-02-23 17:08:57
+ * @FilePath: /auto_deploy/Readme.md
+-->
+## 1、安装包的方式
+
+`yarn`
---
+++ deploy8502.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: ArdenZhao
+ * @Date: 2022-02-23 17:10:13
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2022-02-23 17:11:16
+ * @FilePath: /auto_deploy/deploy8502.js
+ */
+var shell = require('shelljs')
+shell.cd('/Users/zhaoarden/Documents/SpeechOcean/project/ocr/code/ocr/'); // 进入项目目录
